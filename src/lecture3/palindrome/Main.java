d3d999583f4bdf057850f724e2e7bb9711682ce0
AliaksandrTkachenka <aliaksandr.tkachenka@gmail.com>
2017-09-26 14:04:46 +0300

    palindrome with comments

--- src/lecture3/palindrome/Main.java
+++ src/lecture3/palindrome/Main.java
@@ -2,14 +2,22 @@ package lecture3.palindrome;
 
 import java.util.Scanner;
 
+/**
+ * Splitting an input string by the pattern and checking it whether it is a palindrome
+ */
 public class Main {
     public static void main(String[] args) {
+        //Ввод строки с клавиатуры
         Scanner scanner = new Scanner(System.in);
         String string = scanner.nextLine();
+        //Шаблон - всё, кроме букв для разбиения именно на слова
         String pattern = "[^a-zA-Zа-яА-Я]";
+        //Разбиваем строку
         String[] stringArray = string.split(pattern);
+        //Проходим циклом foreach по массиву строк, полученных разбиением исходной строки
         for (String outString: stringArray) {
             StringBuilder stringBuilder = new StringBuilder(outString);
+            //Проверяем, совпадает ли она со своим отражением
             if(outString.equalsIgnoreCase(stringBuilder.reverse().toString())) {
                 System.out.println(outString);
             }
